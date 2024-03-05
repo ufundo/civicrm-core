e34a69d09437000d10159ad0b262cfde91785d9c
benjamin <ufundo@gmail.com>
2024-03-05 15:24:54 +0000

    add CaseRole pseudo-bridge-entity

---
+++ ext/civi_case/Civi/Api4/CaseRole.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+namespace Civi\Api4;
+
+/**
+ * CaseRoleCache - API wrapper on RelationshipCache to facilitate joining and finding contacts with a role on a case
+ *
+ * @searchable secondary
+ * @searchFields contact_a_id.sort_name,relationship_type_id:label,case_id.subject
+ * @see \Civi\Api4\Relationship
+ * @ui_join_filters near_relation
+ * @since 5.72
+ * @package Civi\Api4
+ */
+class CaseRole extends Generic\AbstractEntity {
+  use Generic\Traits\EntityBridge;
+
+  protected static function getEntityTitle(bool $plural = FALSE): string {
+    return $plural ? ts('Case Roles') : ts('Case Role');
+  }
+
+#  /**
+#   * @param bool $checkPermissions
+#   * @return Generic\DAOGetAction
+#   */
+#  public static function get($checkPermissions = TRUE) {
+#    return (new Action\CaseRoleCache\Get(__CLASS__, __FUNCTION__))
+#      ->setCheckPermissions($checkPermissions);
+#  }
+
+#  /**
+#   * @param bool $checkPermissions
+#   * @return Generic\DAOGetFieldsAction
+#   */
+#  public static function getFields($checkPermissions = TRUE) {
+#    return (new Action\CaseRoleCache\GetFields(__CLASS__, __FUNCTION__))
+#      ->setCheckPermissions($checkPermissions);
+#  }
+
+  /**
+   * @param bool $checkPermissions
+   * @return Generic\DAOGetAction
+   */
+  public static function get($checkPermissions = TRUE) {
+    return (new Generic\DAOGetAction(__CLASS__, __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
+
+  /**
+   * @param bool $checkPermissions
+   * @return Generic\DAOGetFieldsAction
+   */
+  public static function getFields($checkPermissions = TRUE) {
+    return (new Generic\DAOGetFieldsAction(__CLASS__, __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
+
+  /**
+   * @return array
+   */
+  public static function getInfo() {
+    $info = parent::getInfo();
+    $info['bridge_title'] = ts('Case Roles');
+    $info['bridge'] = [
+      'contact_id_a' => [
+        'to' => 'case_id',
+        'label' => ts('Contact Roles'),
+        'description' => ts('Contacts with a role in the this case'),
+      ],
+    ];
+    $info['bridge']['case_id'] = [
+    'to' => 'contact_id_a',
+    'label' => ts('Case Roles'),
+    'description' => ts('Cases in which this contact has a role'),
+    ];
+    return $info;
+  }
+
+}
