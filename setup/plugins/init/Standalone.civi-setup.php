1c041fc82f88e7fed946cb9bf62ae9e7e307299e
benjamin <ufundo@gmail.com>
2024-03-29 11:40:17 +0000

    create a helper class for installer path settings

--- setup/plugins/init/Standalone.civi-setup.php
+++ setup/plugins/init/Standalone.civi-setup.php
@@ -9,16 +9,7 @@ if (!defined('CIVI_SETUP')) {
   exit("Installation plugins must only be loaded by the installer.\n");
 }
 
-function _standalone_setup_scheme(): string {
-  if ((!empty($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https') ||
-    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ||
-    (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')) {
-    return 'https';
-  }
-  else {
-    return 'http';
-  }
-}
+
 
 \Civi\Setup::dispatcher()
   ->addListener('civi.setup.checkAuthorized', function (\Civi\Setup\Event\CheckAuthorizedEvent $e) {
@@ -40,90 +31,26 @@ function _standalone_setup_scheme(): string {
     }
     \Civi\Setup::log()->info(sprintf('[%s] Handle %s', basename(__FILE__), 'init'));
 
-    // error_log('artfulrobot: ' . __FILE__ . ' listener for civi.setup.init');
-    // Compute settingsPath.
-    // We use this structure: /var/www/standalone/data/{civicrm.settings.php,templates_c}
-    // to reduce the number of directories that admins have to chmod
+    // @todo why is this set here as well as database.civi-setup.php?
+    // (in order to set cms db as well?)
+    $dbHost  = getenv('CIVICRM_DB_HOST') ?: '127.0.0.1';
+    $dbPort  = getenv('CIVICRM_DB_PORT') ?: '3306';
+    $model->db = $model->cmsDb = [
+      'server' => $dbHost . ':' . $dbPort,
+      'username' => getenv('CIVICRM_DB_USER') ?: '',
+      'password' => getenv('CIVICRM_DB_PASS') ?: '',
+      'database' => getenv('CIVICRM_DB_NAME') ?: '',
+    ];
 
     /**
-     * @var string $projectRootPath
-     *       refers to the root of the *application*, not the actual webroot as reachable by http.
-     *       Typically, this means that $projectRootPath might be like /var/www/example.org/ and
-     *       the actual web root would be /var/www/example.org/web/
-     */
-    if (!empty($model->extras['standaloneRoot'])) {
-      $projectRootPath = $model->extras['standaloneRoot'];
-    }
-    else {
-      $candidates = [
-        // Ex: Clone ~/src/civicrm-core; use PHP built-in server and standalone.
-        $model->srcPath . '/srv',
+    * @todo overwrites any pre-existing settings on $model - is this ok?
+    * use a helper class to get all of the paths and urls
+    * sourcing fromm env vars if set or falling back to opinionated defaults
+    */
+    $pathSettingsHelper = \Civi\Setup\StandalonePathSettings::defaultSetup();
 
-        // Ex: Make a vhost and clone `civicrm-core` as `HTTP_ROOT/core`
-        dirname($model->srcPath, 2),
-
-        // Ex: Clone `civicrm-standalone` which depends on `civicrm-core`. Use Apache/nginx/etc.
-        dirname($model->srcPath, 3),
-      ];
-      foreach ($candidates as $candidate) {
-        if (file_exists($candidate . '/civicrm.config.php.standalone')) {
-          $projectRootPath = $model->extras['standaloneRoot'] = $candidate;
-          break;
-        }
-      }
-    }
-    if (empty($projectRootPath)) {
-      throw new \RuntimeException("Failed to identify standalone root. (TIP: Set extras.standaloneRoot)");
-    }
-    $model->paths['civicrm.private']['path'] = implode(DIRECTORY_SEPARATOR, [$projectRootPath, 'data']);
-    $model->settingsPath = implode(DIRECTORY_SEPARATOR, [$projectRootPath, 'data', 'civicrm.settings.php']);
-    $model->templateCompilePath = implode(DIRECTORY_SEPARATOR, [$projectRootPath, 'data', 'templates_c']);
-    // print "\n-------------------------\nSet model values:\n" . json_encode($model->getValues(), JSON_PRETTY_PRINT) . "\n-----------------------------\n";
-
-    // Compute DSN.
-    // print "=======================\n". json_encode(['model' => $model->getValues(), 'server' => $_SERVER], JSON_PRETTY_PRINT) ."\n";
-    $model->db = $model->cmsDb = [
-      'server' => 'localhost',
-      'username' => '',
-      'password' => '',
-      'database' => '',
-    ];
-
-    // Compute URLs (@todo?)
-    // original: $model->cmsBaseUrl = $_SERVER['HTTP_ORIGIN'] ?: $_SERVER['HTTP_REFERER'];
-    if (empty($model->cmsBaseUrl)) {
-      // A buildkit install (which uses cv core:install) sets this correctly. But a standard composer-then-website type install does not.
-      $model->cmsBaseUrl = _standalone_setup_scheme() . '://' . $_SERVER['HTTP_HOST'];
-    }
-
-    // These paths get set as
-    // $civicrm_paths[k]['url'|'path'] = v
-    $model->paths['cms.root'] = [
-      'path' => $projectRootPath . DIRECTORY_SEPARATOR . 'web',
-    ];
-    $model->paths['civicrm.files'] = [
-      'path' => rtrim($projectRootPath . DIRECTORY_SEPARATOR . 'web') . DIRECTORY_SEPARATOR . 'upload',
-      'url' => $model->cmsBaseUrl . '/upload',
-    ];
+    $pathSettingsHelper->setPathsOnInstallerModel($model);
 
     // Compute default locale.
     $model->lang = $_REQUEST['lang'] ?? 'en_US';
-
-    if (\Composer\InstalledVersions::isInstalled('civicrm/civicrm-asset-plugin')) {
-      $model->mandatorySettings['userFrameworkResourceURL'] = $model->cmsBaseUrl . '/assets/civicrm/core';
-      // civicrm-asset-plugin will fill-in various $paths.
-    }
-    else {
-      $model->mandatorySettings['userFrameworkResourceURL'] = $model->cmsBaseUrl . '/core';
-      $model->paths['civicrm.core']['url'] = $model->cmsBaseUrl . '/core';
-      $model->paths['civicrm.core']['path'] = $model->srcPath;
-      $model->paths['civicrm.vendor']['url'] = $model->cmsBaseUrl . '/core/vendor';
-      $model->paths['civicrm.vendor']['path'] = $model->srcPath . '/vendor';
-      $model->paths['civicrm.bower']['url'] = $model->cmsBaseUrl . '/core/bower_components';
-      $model->paths['civicrm.bower']['path'] = $model->srcPath . '/bower_components';
-      $model->paths['civicrm.packages']['url'] = $model->cmsBaseUrl . '/core/packages';
-      $model->paths['civicrm.packages']['path'] = file_exists($model->srcPath . '/packages')
-          ? $model->srcPath . '/packages'
-          : dirname($model->srcPath) . '/civicrm-packages';
-    }
   });
---
+++ setup/src/Setup/StandalonePathSettings.php
@@ -0,0 +1,326 @@
+<?php
+
+namespace Civi\Setup;
+
+/**
+ * Opinionated helper for configuring default paths and urls in standalone (could be generally usable?)
+ *
+ *
+ * note: paths generally get set as $civicrm_paths[k]['url'|'path'] = v
+ * though there are exceptions (private directory does not have a url, some paths are specified directly in old config)
+ *
+ * default structure uses project root = web root (e.g. /var/www/html)
+ * and then:
+ * ./private (private files)
+ * ./private/civicrm.settings.php (civicrm site settings file)
+ * ./private/cached_templates (template compilation directory)
+ * ./private/log
+ * ./public (public files)
+ *
+ * @see https://lab.civicrm.org/dev/core/-/issues/5073
+ */
+
+ class StandalonePathSettings {
+
+  public const DEFAULT_PATHS = [
+    // default from PHP SERVER VARS
+    'project_root' => null,
+
+    // private
+    'private'      => '[project_root]/private',
+    // private subdirs
+    'settings'     => '[private]/civicrm.settings.php',
+    'compile'      => '[private]/cached_templates',
+    'private_uploads'       => '[private]/uploads',
+    'tmp'          => '[private]/tmp',
+    'log'          => '[private]/log',
+    'l10n'         => '[private]/translations',
+  ];
+
+  public const DEFAULT_WEB_PATHS = [
+    // default from PHP SERVER VARS
+    'web_root'    => null,
+
+    // public (note: "files" in $m->paths)
+    'public'      => '[web_root]/public',
+    // public subdirs
+    'public_uploads'     => '[public]/uploads',
+    // extensions
+    'extensions'  => '[web_root]/extensions',
+
+    // core
+    'core'        => '[web_root]/core',
+    // core subdirs
+    'bower'       => '[core]/bower_components',
+    'vendor'      => '[core]/vendor',
+    'packages'    => '[core]/packages',
+    'setup'       => '[core]/setup',
+  ];
+
+  protected array $paths = [];
+
+//  public function __construct(string $projectRootPath, ?string $webRootPath, string $webRootUrl)
+//  {
+//    $this->projectRootPath = $projectRootPath;
+//    $this->webRootUrl = $webRootUrl;
+//    $this->webRootPath = $webRootPath ?? $projectRootPath;
+//  }
+
+  protected function setPath(string $key, string $value)
+  {
+    // replace tokens before setting value
+    $value = $this->replacePathTokens($value);
+
+    // clean path string
+    $value = str_replace('/', DIRECTORY_SEPARATOR, $value);
+    $value = rtrim($value, DIRECTORY_SEPARATOR);
+
+    if (!$value) {
+      throw new \ValueError('Cannot set empty string path for key: ' . $key);
+    }
+
+    $this->paths[$key] = $this->paths[$key] ?? [];
+    $this->paths[$key]['path'] = $value;
+  }
+
+  public function getPath(string $key): string
+  {
+    $path = $this->paths[$key]['path'] ?? null;
+    if (!$path) {
+      throw new \Exception('No path set for key: ' . $key);
+    }
+    return $path;
+  }
+
+  /**
+   * for a path string
+   * - replace [token] with path from $this->buildPaths
+   *
+   * @return string
+   * @throws \Exception if a token that isn't already defined is used
+   */
+  protected function replacePathTokens(string $path): string
+  {
+    // replace any parts which are already set tokens
+    foreach (array_keys($this->paths) as $key) {
+      $token = '[' . $key . ']';
+      $tokenValue = $this->getPath($key);
+
+      $path = str_replace($token, $tokenValue, $path);
+    }
+
+    // check for outstanding tokens
+    if (preg_match('/\[.+\]/', $path)) {
+      throw new \Exception("Couldn't replace all tokens in path: " . $path);
+    }
+
+    return $path;
+  }
+
+  protected function pathFromEnvVar(string $key): ?string
+  {
+    $path = getenv('CIVICRM_PATH_' . strtoupper($key));
+
+    return $path ?: null;
+  }
+
+  protected function pathFromDefault($key): ?string
+  {
+    if ($key === 'project_root') {
+      $path = $_SERVER['SERVER_ROOT'] ?? $_SERVER['DOCUMENT_ROOT'] ?? null;
+    }
+    else if ($key === 'web_root') {
+      $path = $_SERVER['DOCUMENT_ROOT'] ?? $this->getPath('project_root');
+    }
+    else {
+      $path = self::DEFAULT_PATHS[$key] ?? self::DEFAULT_WEB_PATHS[$key] ?? null;
+    }
+
+    return $path ?: null;
+  }
+
+  protected function setPathFromEnvVarOrDefault(string $key)
+  {
+    $path = $this->pathFromEnvVar($key) ?: $this->pathFromDefault($key);
+
+    if (!$path) {
+      $envVarName = 'CIVICRM_PATH_' . strtoupper($key);
+      throw new \Exception("Couldn't determine path for '{$key}'. You can set explicitly using environment variable {$envVarName}");
+    }
+
+    $this->setPath($key, $path);
+  }
+
+
+  protected function urlFromEnvVar(string $key): ?string
+  {
+    return getenv('CIVICRM_URL_' . strtoupper($key)) ?: null;
+  }
+
+
+  protected function setUrl(string $key, string $value)
+  {
+    $value = rtrim($value, '/');
+
+    if (!$value) {
+      throw new \ValueError('Cannot set empty string url for key: ' . $key);
+    }
+    $this->paths[$key] = $this->paths[$key] ?? [];
+    $this->paths[$key]['url'] = $value;
+  }
+
+  public function getUrl(string $key): string
+  {
+    $url = $this->paths[$key]['url'] ?? null;
+    if (!$url) {
+      throw new \Exception('No url set for key: ' . $key);
+    }
+    return $url;
+  }
+
+
+  protected function deriveWebRoot(): string
+  {
+    return ($this->schemeFromEnvOrServerVars() . '://' . $this->hostFromEnvOrServerVars());
+  }
+
+  protected function hostFromEnvOrServerVars(): string
+  {
+    return getenv('CIVICRM_SITE_HOST') ?: $_SERVER['HTTP_HOST'];
+  }
+
+  /**
+   * @todo this might fail behind a proxy?
+   */
+  protected function schemeFromEnvOrServerVars(): string
+  {
+    $schemeFromEnvVar = getenv('CIVICRM_SITE_SCHEME');
+
+    if ($schemeFromEnvVar) {
+      return $schemeFromEnvVar;
+    }
+
+    if ((!empty($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https') ||
+      (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ||
+      (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')) {
+      return 'https';
+    }
+
+    return 'http';
+  }
+
+  /**
+   * @throws \Exception if the target path isnt below the web root
+   */
+  protected function deriveUrlFromWebRoot(string $targetPathKey): string
+  {
+    $webRootPath = $this->getPath('web_root');
+    $targetPath = $this->getPath($targetPathKey);
+
+    // check target path is inside the webroot
+    // (it might be valid to use a directory outside the webroot if doing something clever, but you'll need to set the URL directly)
+    if (strpos($targetPath, $webRootPath) !== 0) {
+      $urlEnvVar = 'CIVICRM_URL_' . strtoupper($targetPathKey);
+      throw new \Exception("Couldn't determine relative path from web_root ('{$webRootPath}') to {$targetPathKey} ('{$targetPath}') in order to derive the URL - please ensure one path under the other or set {$urlEnvVar} directly");
+    }
+
+    $relativePath = substr($targetPath, strlen($webRootPath));
+
+    return $this->getUrl('web_root') . str_replace(DIRECTORY_SEPARATOR, '/', $relativePath);
+  }
+
+  protected function setUrlFromEnvVarOrDerive(string $key)
+  {
+    $url = $this->urlFromEnvVar($key);
+
+    if (!$url) {
+      $url = ($key === 'web_root') ? $this->deriveWebRoot() : $this->deriveUrlFromWebRoot($key);
+    }
+
+    $this->setUrl($key, $url);
+  }
+
+  public static function setup(array $pathKeysToSetInOrder, array $urlKeysToSetInOrder): StandalonePathSettings
+  {
+    $settings = new self();
+
+    foreach ($pathKeysToSetInOrder as $key) {
+      $settings->setPathFromEnvVarOrDefault($key);
+    }
+
+    foreach ($urlKeysToSetInOrder as $key) {
+      $settings->setUrlFromEnvVarOrDerive($key);
+    }
+
+    return $settings;
+  }
+
+  public static function defaultSetup(): StandalonePathSettings
+  {
+    $pathKeys = array_keys(self::DEFAULT_PATHS + self::DEFAULT_WEB_PATHS);
+    $urlKeys = array_keys(self::DEFAULT_WEB_PATHS);
+
+    return self::setup($pathKeys, $urlKeys);
+  }
+
+  public function getCorePathConfig()
+  {
+    $keyMap = [
+      'cms.root' => 'web_root',
+      'civicrm.private' => 'private',
+      'civicrm.files' => 'public_uploads',
+      'civicrm.l10n' => 'l10n',
+      'civicrm.tmp' => 'tmp',
+      'civicrm.custom' => 'private_uploads',
+      'civicrm.log' => 'log',
+    ];
+
+    $mapped = [];
+
+    foreach ($keyMap as $theirKey => $ourKey) {
+      $mapped[$theirKey] = $this->paths[$ourKey];
+    }
+
+    return $mapped;
+  }
+
+  public function getDomainLevelPathSettings()
+  {
+    $userFrameworkResourceUrl = (\Composer\InstalledVersions::isInstalled('civicrm/civicrm-asset-plugin'))
+      ? $this->getUrl('public') . '/assets/civicrm/core' : $this->getUrl('core');
+
+    return [
+      'extensionsDir' => $this->getPath('extensions'),
+      'extensionsURL' => $this->getUrl('extensions'),
+      'imageUploadDir' => $this->getPath('public_uploads'),
+      'imageUploadURL' => $this->getUrl('public_uploads'),
+      'uploadDir' => $this->getPath('tmp'),
+      'customFileUploadDir' => $this->getPath('private_uploads'),
+      'userFrameworkResourceURL' => $userFrameworkResourceUrl,
+    ];
+  }
+
+  /**
+   * this is mainly to ensure "old style" settings are set with their direct properties
+   * @todo overrides any existing values - is this ok?
+   */
+  public function setPathsOnInstallerModel(&$model)
+  {
+    $model->paths = $this->getCorePathConfig();
+
+    $model->mandatorySettings = $this->getDomainLevelPathSettings();
+
+    // set old style settings on the model as well
+    $model->cmsBaseUrl = $this->getUrl('web_root');
+    $model->templateCompilePath = $this->getPath('compile');
+    $model->customFileUploadDir = $this->getPath('private_uploads');
+    $model->uploadDir = $this->getPath('tmp');
+    $model->settingsPath = $this->getPath('settings');
+    $model->imageUploadDir = $this->getPath('public_uploads');
+    $model->extensionsDir = $this->getPath('extensions');
+
+    $model->srcPath = $this->getPath('core');
+    $model->setupPath = $this->getPath('setup');
+  }
+
+ }
