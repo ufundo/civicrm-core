53398843cec2aa36a185c1fa3c339ffe1e958966
Vangelis Pantazis <v.pantazis@ixiam.com>
2026-04-21 01:24:26 +0300

    fixed indentation error

--- ext/riverlea/Civi/Api4/Action/RiverleaStream/Render.php
+++ ext/riverlea/Civi/Api4/Action/RiverleaStream/Render.php
@@ -99,7 +99,7 @@ class Render extends \Civi\Api4\Generic\BasicBatchAction {
       return $stream['dark_frontend'] ?? \Civi::settings()->get('riverlea_dark_mode_frontend');
     }
     else {
-         // Backend requests may be overridden by user cookie
+      // Backend requests may be overridden by user cookie
       if ((bool) \Civi::settings()->get('riverlea_dark_mode_toggle_backend') && !empty($_COOKIE['riverlea_dark_mode_backend'])) {
         $cookieVal = $_COOKIE['riverlea_dark_mode_backend'];
         return in_array($cookieVal, ['light', 'dark', 'inherit'], TRUE) ? $cookieVal : ($stream['dark_backend'] ?? \Civi::settings()->get('riverlea_dark_mode_backend'));
