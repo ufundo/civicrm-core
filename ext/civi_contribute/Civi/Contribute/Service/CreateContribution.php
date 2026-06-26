d30ec73495ba37079c36db4d59a7e6cf5f6ed995
benjamin <ben@civicrm.org>
2026-06-26 13:53:29 +0100

    wip checkout recurring pp link

--- ext/civi_contribute/Civi/Contribute/Service/CreateContribution.php
+++ ext/civi_contribute/Civi/Contribute/Service/CreateContribution.php
@@ -188,7 +188,7 @@ class CreateContribution extends AutoService implements EventSubscriberInterface
     $event->setEntityId(0, $savedContribution['id']);
 
     if ($contribution['recur_period'] ?? NULL) {
-      $this->createContributionRecur($savedContribution['id'], $contribution['recur_period']);
+      $this->createContributionRecur($savedContribution['id'], $contribution['recur_period'], $contribution['checkout_option'] ?? NULL);
     }
 
   }
@@ -216,7 +216,7 @@ class CreateContribution extends AutoService implements EventSubscriberInterface
   /**
    * For a recurring contribution, create a ContributionRecur record as well
    */
-  public function createContributionRecur(int $contributionId, string $recurPeriod) {
+  public function createContributionRecur(int $contributionId, string $recurPeriod, ?string $checkoutOption = NULL) {
     // get values we need to reuse from the contribution record
     $contribution = \Civi\Api4\Contribution::get(FALSE)
       ->addSelect('contact_id', 'total_amount', 'currency', 'is_test')
@@ -241,6 +241,18 @@ class CreateContribution extends AutoService implements EventSubscriberInterface
     // calculate the next scheduled date
     $nextSched = (new DateTime("+ {$recurParams['frequency_interval']} {$recurParams['frequency_unit']}"))->format('Y-m-d');
 
+    if ($checkoutOption) {
+      $checkoutOption = \Civi::service('civi.checkout')->getOption($checkoutOption);
+      $paymentProcessorId = $checkoutOption->getPaymentProcessorId();
+      // TODO: pass test mode to get the test mode processor ID? but *which* test mode?
+      // $testMode = \Civi::service('civi.checkout')->isTestMode();
+      // $testMode = $contribution['is_test'];
+      // $paymentProcessorId = $checkoutOption->getPaymentProcessorId($testMode);
+    }
+    else {
+      $paymentProcessorId = NULL;
+    }
+
     $recurRecordId = \Civi\Api4\ContributionRecur::create(FALSE)
       ->addValue('contact_id', $contribution['contact_id'])
       ->addValue('amount', $contribution['total_amount'])
@@ -249,6 +261,7 @@ class CreateContribution extends AutoService implements EventSubscriberInterface
       ->addValue('frequency_unit', $recurParams['frequency_unit'])
       ->addValue('frequency_interval', $recurParams['frequency_interval'])
       ->addValue('next_sched_contribution_date', $nextSched)
+      ->addValue('payment_processor_id', $paymentProcessorId)
       ->execute()
       ->single()['id'];
 
